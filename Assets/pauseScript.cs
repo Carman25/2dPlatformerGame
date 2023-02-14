03a1064d284e9169a594ab2acc8a42f13dbb97d9
Carman25 <carson.whitehouse@gmail.com>
2023-02-14 11:40:15 -0500

    Extra pause stuff

--- Assets/pauseScript.cs
+++ Assets/pauseScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class pauseScript : MonoBehaviour
 {
+    private bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if()
     }
 }
